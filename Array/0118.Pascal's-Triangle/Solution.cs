420b41e80384b5681b2cac9e7b3f0858452da189
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2023-01-10 16:00:13 +0100

    Update Solution.cs

--- Array/0118.Pascal's-Triangle/Solution.cs
+++ Array/0118.Pascal's-Triangle/Solution.cs
@@ -16,3 +16,23 @@ public class Solution {
         return triangle;
     }
 }
+
+/// alternative approach
+
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        
+        IList<IList<int>> triangle = new List<IList<int>>(){new int[1]{1}};      
+        
+        for (int row = 0; row < numRows-1; ++row)
+        {
+            triangle.Add(new int[row+2]);
+            for (int j = 0; j <= row; ++j) {
+                triangle[row+1][j] += triangle[row][j];
+                triangle[row+1][j+1] += triangle[row][j];
+            }
+        } 
+        
+        return triangle;
+    }
+}
