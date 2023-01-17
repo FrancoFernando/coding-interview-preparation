8da9018ca3aa98b7b673b52b6baebf292c384676
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2023-01-17 22:26:42 +0100

    Update Solution.cs

--- Array/0349.Intersection-of-Two-Arrays/Solution.cs
+++ Array/0349.Intersection-of-Two-Arrays/Solution.cs
@@ -16,3 +16,10 @@ public class Solution {
         return intersection.ToArray();
     }
 }
+
+//LINQ
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        return nums1.Intersect(nums2).ToArray();
+    }
+}
