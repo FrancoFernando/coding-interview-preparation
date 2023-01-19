74ab4ec0f92c04a4765e8fa58da73d02e4c6eec4
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2023-01-19 09:13:07 +0100

    Update Solution.cs

--- Array/724.Find-Pivot-Index/Solution.cs
+++ Array/724.Find-Pivot-Index/Solution.cs
@@ -1,20 +1,14 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
         
-        var prefixSum = new int[nums.Length+2];
+        int prefixSum = 0, postfixSum = nums.Sum();
 
-        for (int i = 1; i <= nums.Length; ++i) {
-            prefixSum[i] = prefixSum[i-1] + nums[i-1]; 
-        }
-
-        for (int i = 1; i <= nums.Length; ++i) {
-            if (prefixSum[i-1] == prefixSum[nums.Length]-prefixSum[i]) {
-                return i-1;
-            }
+        for (int i = 0; i < nums.Length; ++i) {
+            postfixSum -= nums[i];
+            if (prefixSum == postfixSum) return i;
+            prefixSum += nums[i];
         }
 
         return -1;
     }
 }
-
-//Optimized: need only total sum, and sumleft, no need of additional O(N) space
