668ee27d10ad3cdf0330abf81d1062eb59f05755
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2023-01-24 22:30:22 +0100

    Update Solution.cs

--- LinkedList/0234.Palindrome-Linked-List/Solution.cs
+++ LinkedList/0234.Palindrome-Linked-List/Solution.cs
@@ -10,7 +10,7 @@
  * }
  */
 
-// recursive solution
+// Recursive solution
 public class Solution {
 
     public bool IsPalindromeHelper(ListNode currentNode, ref ListNode oppositeNode) {
@@ -24,5 +24,61 @@ public class Solution {
     public bool IsPalindrome(ListNode head) {
         ListNode oppositeNode = head;
         return IsPalindromeHelper(head, ref oppositeNode);
+    }
+}
+
+// Optimized solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public ListNode FindMiddleNode(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode ReverseList(ListNode head) {
+
+        ListNode prev = null;
+        while(head != null) {
+            var next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode head, ListNode tail) {
+
+        while (head != null && tail != null) {
+            if (head.val != tail.val) return false;
+            head = head.next;
+            tail = tail.next;
+        }
+        return true;
+    }
+
+    public bool IsPalindrome(ListNode head) {
+
+        var middleNode = FindMiddleNode(head);
+        var tailNode = ReverseList(middleNode);
+        return isPalindrome(head, tailNode);
     }
 }
