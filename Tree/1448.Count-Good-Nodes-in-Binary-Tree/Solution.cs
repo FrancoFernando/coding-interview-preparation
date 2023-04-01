4bb6f226a74a17e9cf56c625da71a645ad0e1bc6
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2023-04-01 10:55:16 +0200

    Create Solution.cs

---
+++ Tree/1448.Count-Good-Nodes-in-Binary-Tree/Solution.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+
+    private int dfs (TreeNode node, int maxValFromRootToNode) {
+        
+        if (node is null) return 0;
+
+        int goodNodesInSubtree = 0;
+
+        if (node.val >= maxValFromRootToNode) {
+            goodNodesInSubtree++;
+            maxValFromRootToNode = node.val;
+        }
+
+        goodNodesInSubtree += dfs(node.left, maxValFromRootToNode);
+        goodNodesInSubtree += dfs(node.right, maxValFromRootToNode);
+
+        return goodNodesInSubtree;
+    }
+
+    public int GoodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+}
+
+//the path from root to x shall be monotonically increasing
+//1. dfs keeping the max alomng the path and returning the count
