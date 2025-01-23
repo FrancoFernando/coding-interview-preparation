4c779d97c3d568ee5ae40670f216c138004908fa
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2025-01-23 20:10:51 +0100

    Update Solution.cs

--- leetcode75/338. Counting Bits/Solution.cs
+++ leetcode75/338. Counting Bits/Solution.cs
@@ -22,3 +22,23 @@ public class Solution {
         return ones;
     }
 }
+
+//O(N) solution
+public class Solution {
+    public int[] CountBits(int n) {
+        
+        var result = new int[n+1]; 
+
+        for (int i = 1, nextPower = 2; i <= n; ++i) {
+            if (i == nextPower) {
+                result[i] = 1;
+                nextPower *= 2;
+            }
+            else {
+                result[i] = result[i >> 1] + (i & 1);
+                //result[i] = result[i/2] + (i % 2);
+            }
+        }
+        return result;
+    }
+}
