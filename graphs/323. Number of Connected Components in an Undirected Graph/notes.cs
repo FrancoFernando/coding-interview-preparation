90b303bc4ff0e57167dbff48806457dc88768cac
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2025-02-04 16:16:55 +0100

    Create notes.cs

---
+++ graphs/323. Number of Connected Components in an Undirected Graph/notes.cs
@@ -0,0 +1,28 @@
+# Description
+
+You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
+
+ 
+
+Example 1:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+Example 2:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
