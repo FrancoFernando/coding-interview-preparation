aec46502d9288a822cdcd29b1d98741125569053
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2025-07-01 16:44:37 +0200

    Update solution.py

--- linked-lists/203. Remove Linked List Elements/solution.py
+++ linked-lists/203. Remove Linked List Elements/solution.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 # Recursive
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
@@ -15,3 +16,18 @@ class Solution:
 
         head.next = self.removeElements(head.next, val);
         return head;
+        
+#iterative
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+
+        dummy = ListNode(0, head)
+        current = dummy
+        
+        while current.next is not None:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummy.next;
